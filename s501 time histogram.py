8ac814aebde3b1923889f1c8136a9489f40b2ff2
tomkooij <tomkooij@tomkooij.nl>
2014-09-30 14:45:53 +0200

    April 2010 data

--- s501 time histogram.py
+++ s501 time histogram.py
@@ -2,6 +2,7 @@
 Read data from station 501 and plot t1-t2 histogram
 
 Goal: recreate graphs form D.Pennink 2010
+t1-t2 from station 501, during April 2010
 
 29sep: created
 29sep: TODO consider dt = t1-t2; select events based on ph2 and/or ph2 (cp-gamma, gamma-cp and/or gamma-gamma) plot histogram and fit gaussian
@@ -9,7 +10,9 @@ Goal: recreate graphs form D.Pennink 2010
 ph1 > TRIGGER = charged particle
 ph1 < TRIGGER = gamma
 
-Find out value of TRIGGER (<120 and >200 volgens LIO verslag)
+Find out value of TRIGGER (<120 and >200 ADC counts volgens LIO verslag)
+Zelf uitgetest: sigma=9ns volgt bij HIGH_PH = 800.
+PROBLEEM: Hoe hoger HIGH_PH hoe smaller de verdeling!!!!
 """
 
 import tables
@@ -17,11 +20,11 @@ import sapphire.esd
 import scipy.stats 
 
 STATIONS = [501]
-START = datetime.datetime(2014,8,1)
-END = datetime.datetime(2014,9,1)
-FILENAME = 'station_501_augustus2014.h5'
+START = datetime.datetime(2010,4,1)
+END = datetime.datetime(2010,5,1)
+FILENAME = 'station_501_april2010.h5'
 
-HIGH_PH = 200
+HIGH_PH = 820 # fitted with april 2010 data
 LOW_PH = 120
 
 #
@@ -43,7 +46,7 @@ def create_new_event_file(filename, stations, start, end):
 #
 # Open existing coincidence table. 
 # Only check if "/coincidences" are in table, no other checks
-def open_existing_events_file(filename):
+def open_existing_event_file(filename):
     data = tables.open_file(FILENAME, 'a')
     return data
 
@@ -78,7 +81,11 @@ def plot_dt_histogram_ph(tA,tB):
 
    
     
-data = open_existing_events_file(FILENAME)
+#data = create_new_event_file(FILENAME, STATIONS, START, END)
+#data.close()
+    
+data = open_existing_event_file(FILENAME)
+
 events = data.root.s501.events
     
 t1 = events.col('t1')
@@ -92,7 +99,7 @@ ph2 = ph[:,1]
 ph3 = ph[:,2]
 ph4 = ph[:,3]
     
-bins2ns5 = arange(-101.25,101.26,2.5)
+bins2ns5 = arange(-201.25,202.26,2.5)
     
 #
 # Plot histogram for tA-tB using hardcoded event selection based on pulseheight
@@ -102,15 +109,34 @@ dt = t1 - t2
     
 # remove -1 and -999
 # select events based on pulseheight    
-fixed_dt = dt.compress((t1 >= 0) & (t2 >= 0) & (ph1 < 120) & (ph2 < 120))
+fixed_dt = dt.compress((t1 >= 0) & (t2 >= 0) & (ph1 > HIGH_PH) & (ph2 > HIGH_PH))
 print "number of events: %d" % len (fixed_dt)
     
-hist(fixed_dt, bins=bins2ns5, normed=1)
+hist(fixed_dt, bins=bins2ns5)
 (mu, sigma) = scipy.stats.norm.fit(fixed_dt)
         
-print "avg: %f, sigma: %f" % (mu,sigma)
+print "avg: %f, sigma: %f" % (mu,sigma**0.5)
 
 fitted_pdf = scipy.stats.norm.pdf(bins2ns5, mu, sigma)
 plot(bins2ns5, fitted_pdf)
+
+def gauss(data):
+    
+    X = arange(data.size)
+    x = sum(X*data)/sum(data)
+    sigma = sqrt(abs(sum((X-x)**2*data)/sum(data)))
+     
+    scale = data.max()
+ 
+    return x, sigma, scale
+
+(x,sigma, scale) = gauss(fixed_dt)
+
+print (x,sigma,scale)    
+X = arange(fixed_dt.size)    
+fit = lambda t : scale*exp(-(t-x)**2/(2*sigma**2))
+ 
+#plot(fit(bins2ns5)) 
+
 
 #data.close()
