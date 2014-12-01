ebd6023a7f33685630b3a5241d6f6f686357d7dc
tomkooij <tomkooij@tomkooij.nl>
2014-12-01 11:44:06 +0100

    fixed pulseheight histogram from corsika

--- corsika/corsika_pulseintegrals.py
+++ corsika/corsika_pulseintegrals.py
@@ -9,6 +9,8 @@ bron simulaties AdL zie email 1dec2014
 """
 
 import tables
+import numpy as np
+import matplotlib.pyplot as plt
 
 # datafiles (AdL email 1dec2014)
 FILENAME1 = '15_45_simulation.h5'
@@ -16,15 +18,16 @@ FILENAME2 =  '16_45_simulation.h5'
 FILENAME3 = '16_7_5_simulation.h5'
 FILENAME4 = '17_45_simulation.h5'
 
-if __name__='__main__'
+if __name__=='__main__':
 
     print 'pulseheight histograms from CORSIKA sims (using GroundParticleSim output) \n'
-    data = table.file_open(FILENAME1, 'r')
+    data = tables.open_file(FILENAME4, 'r')
 
     events = data.root.cluster_simulations.station_501.events
 
     n1 = events.col('n1') # er is geen pulseintegral data, maar "n" aantal mips is hetzelfde
 
-    bins = arange(0.1, 5.,0.05)
+    bins = np.arange(0.1, 5.,0.05)
 
-    hist(n1, bins)
+    plt.figure()
+    plt.hist(n1, bins)
