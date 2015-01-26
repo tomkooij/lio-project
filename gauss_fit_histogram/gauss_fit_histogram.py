2c2990001935c122a2cc2f37ea71e35f31e552e4
tomkooij <tomkooij@tomkooij.nl>
2015-01-26 13:58:29 +0100

    Added verbose option

--- gauss_fit_histogram/gauss_fit_histogram.py
+++ gauss_fit_histogram/gauss_fit_histogram.py
@@ -18,16 +18,34 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 
-def gauss_fit_histogram(histogram_y, bins, sigma_list):
+def gauss_fit_histogram(histogram_y, bins, sigma=None, initialguess = [1., 1., 1., 0.], verbose=False):
     """
     Fit a histogram with a gaussian distribution
 
     Can fit non-normalised histogram with vertical "noise" offsets
     Fit = a*np.exp(-0.5*((x-b)/c)**2)+d
 
-    :param histogram_y: a list with histogram data
-    :param bins: list with bin-edges [left, left, ... , right]
-    :sigma_list: list with standard deviation sigma per bin
+    Parameters
+    ----------
+
+    histogram_y: a list with histogram data
+    bins: list with bin-edges [left, left, ... , right]
+    sigma:  None or list with uncertaincies in the histrogram_y data
+    initialguess : None initial guess
+
+    Returns
+    -------
+    c : a list of parameters
+    fitx, fity : two lists with (x,y) pairs of the fit for plotting
+    verbose : Boolean: True prints fit info (parameters, co-variance matrix, chi2 )
+
+    Example
+    -------
+    plt.figure()
+    n,bins,patches = plt.hist(dataset,bins=bins_edges,histtype='step')
+    c, fitx, fity = gauss_fit_histogram(n, bins, sigma=sigma_list)
+    plt.plot(fitx, fity ,'r--', linewidth=3)
+    plt.show()
     """
 
     #
@@ -40,14 +58,16 @@ def gauss_fit_histogram(histogram_y, bins, sigma_list):
     #
     middle = [(bins[i]+bins[i+1])/2 for i in range(len(bins)-1)]
 
-    # Least squares: scipy.optimize.curve_fit:
-    c, cov = curve_fit(fit_func, middle, n, sigma=sigma_list, absolute_sigma=True)
 
-    print "exp[-0.5((x-mu)/sigma)^2]"
-    print "Fit Coefficients:"
-    print c[0],c[1],abs(c[2]),c[3]
-    print "Co-variance matrix:"
-    print cov
+    # Least squares: scipy.optimize.curve_fit:
+    c, cov = curve_fit(fit_func, middle, n, p0=initialguess, sigma=sigma_list, absolute_sigma=True)
+
+    if (verbose):
+        print "exp[-0.5((x-mu)/sigma)^2]"
+        print "Fit Coefficients:"
+        print c[0],c[1],abs(c[2]),c[3]
+        print "Co-variance matrix:"
+        print cov
 
     fit =  fit_func(middle, c[0], c[1], abs(c[2]), c[3])
 
@@ -55,11 +75,12 @@ def gauss_fit_histogram(histogram_y, bins, sigma_list):
     # In dit geval is dat de spreiding op de schaal/normalisatie faktor
     # is dat is de juiste?
 
-    chi2 = sum(np.power((n - fit)/sigma_list,2)) / (len(n) - len(c))
-    print "Reduced Chi-squared: ", chi2
+    if (verbose):
+        chi2 = sum(np.power((n - fit)/sigma_list,2)) / (len(n) - len(c))
+        print "Reduced Chi-squared: ", chi2
 
-    pearsson = sum(np.power((n - fit),2)/fit) / (len(n) - len(c))
-    print 'Reduced Pearsons Chi-squared: ', pearsson
+        pearsson = sum(np.power((n - fit),2)/fit) / (len(n) - len(c))
+        print 'Reduced Pearsons Chi-squared: ', pearsson
 
     return c, middle, fit
 
@@ -84,7 +105,7 @@ if __name__=='__main__':
     #
     sigma_list = np.sqrt(n)
 
-    c, fitx, fity = gauss_fit_histogram(n, bins, sigma_list)
+    c, fitx, fity = gauss_fit_histogram(n, bins, sigma=sigma_list, verbose=True)
     mu = c[1]
     simga = abs(c[2])
 
