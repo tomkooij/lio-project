942c925511ac6f9aeabb197d30f600fa521f589e
tomkooij <tomkooij@tomkooij.nl>
2015-02-23 14:11:22 +0100

    gamma simulation test

--- corsika/corsika_run_sim.py
+++ corsika/corsika_run_sim.py
@@ -9,10 +9,13 @@ N = number of simulations
 
 """
 
-max_core_distance = 3000 	# m
-N = 100000					# monte carlo runs
+
+max_core_distance = 30	# m
+N = 1000					# monte carlo runs
 
 import tables
+import matplotlib.pyplot as plt
+import numpy as np
 
 from sapphire.simulations.groundparticles import GroundParticlesSimulation
 from sapphire.clusters import *
@@ -25,16 +28,21 @@ FILENAME = 'corsika_77102826_200916071.h5'     # 1e14 p theta = 22.5
 cluster = SingleStation()
 #cluster = SingleDiamondStation()
 
-
-data = tables.open_file('gp_sim_output.h5', 'w')
-#
-# Use GroundParticlesSimulation --> randomize azimuth and core distance
-# we set max core distance to 0, only azimuth is varied
-#
-sim = GroundParticlesSimulation(FILENAME, max_core_distance, cluster, data, '/simrun', N)
-sim.run()
-print "closing datafile..."
-data.close()
-#data = tables.open_file('gp_sim_output.h5', 'r')
-#
-#
+if __name__ == '__main__':
+    data = tables.open_file('gp_sim_output.h5', 'w')
+    #
+    # Use GroundParticlesSimulation --> randomize azimuth and core distance
+    # we set max core distance to 0, only azimuth is varied
+    #
+    sim = GroundParticlesSimulation(FILENAME, max_core_distance, cluster, data, '/simrun', N)
+    sim.run()
+    print "closing datafile... and opening readonly"
+    data.close()
+    data = tables.open_file('gp_sim_output.h5', 'r')
+    #
+    #
+    events = data.root.simrun.cluster_simulations.station_0.events
+    n1 = events.col('n1')
+    plt.hist(n1, bins=np.arange(0,4.,0.1))
+    plt.show()
+    #data.close()
