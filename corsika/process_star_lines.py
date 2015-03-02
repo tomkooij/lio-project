66462944f6906596cfcc9b6f5f4f50bf9b58b07d
tomkooij <tomkooij@tomkooij.nl>
2015-03-02 15:54:13 +0100

    process corsika_run_sim.py stdout output

---
+++ corsika/process_star_lines.py
@@ -0,0 +1,31 @@
+"""
+# process_star_lines.py
+#
+# lees * regel uit corsika_run_sim.py om photon digitalisatie te analyseren
+#
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+FILENAME = 'output'
+
+#
+# lees * lines uit stdout output van corsika_run_sim.py
+#
+def read_input(filename):
+    photons = []     # array of events
+                    # single event = (E, k, kans, mips)
+
+    lines = [line.strip().split(' ') for line in open(filename)]
+
+    for line in lines:
+        # regels met TK zijn regels uit "do_compton()"
+        if (line[0] == "*"):
+            photons.append(line)
+
+    return photons
+
+if __name__=='__main__':
+    print 'Dit is process_star_lines.py'
+    photons = read_input(FILENAME)
+    print 'E = ',photons[1]
