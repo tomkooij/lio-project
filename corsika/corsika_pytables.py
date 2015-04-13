a7e554c13e4103e146936b0e0d12f1c448015df2
tomkooij <tomkooij@tomkooij.nl>
2015-04-13 17:11:07 +0200

    testcase script

--- corsika/corsika_pytables.py
+++ corsika/corsika_pytables.py
@@ -1,59 +1,76 @@
 # -*- coding: utf-8 -*-
 """
 
-Throw CORSIKA results on a single 4 plate HiSPARC station
-
-Monte Carlo parameters:
-max_core_distance = maximum distance of station to the shower core (randomised each run)
-N = number of simulations
+Investigate Pytables.read_where()/Numpy.compress() performance
 
 """
 
 
 max_core_distance = 400  	# m
-N = 100					# monte carlo runs
+N = 1					# monte carlo runs
 
 import tables
-import matplotlib.pyplot as plt
+from timeit import default_timer as timer
 import numpy as np
 
 from sapphire.simulations.groundparticles import GroundParticlesSimulation
 from sapphire.simulations.groundparticles import OptimizeQueryGroundParticlesSimulation
-from sapphire.simulations.groundparticles import OptimizeQuery_ParticlesOnly_GroundParticlesSimulation
-from sapphire.simulations.groundparticles import NumpyGPS
+#from sapphire.simulations.groundparticles import OptimizeQuery_ParticlesOnly_GroundParticlesSimulation
+#from sapphire.simulations.groundparticles import NumpyGPS
 
 from sapphire.clusters import ScienceParkCluster
 
-#FILENAME = 'corsika_834927089_144221120.h5'    # 1e14 p theta = 0
-#FILENAME = 'corsika_77102826_200916071.h5'     # 1e14 p theta = 22.5
-
-#FILENAME = 'corsika_713335232_854491062.h5'    # 1e14 p theta = 0
-#FILENAME_SORTED = 'sorted.h5' # corsika_713335232_854491062.h5 sorted by x
-FILENAME = 'corsika_10019744_792483217.h5'  # 1e16 theta = 22.5
-FILENAME_SORTED = 'sorted_10019744.h5'
 
 cluster = ScienceParkCluster()
 
 
-if __name__ == '__main__':
+SHOWERS = {'1e14 eV 7Mb unsorted': 'corsika_713335232_854491062.h5',
+            '1e14 eV 6Mb sorted+csi': 'sorted_713335232.h5',
+            '1e16 eV 88Mb unsorted': 'corsika_10019744_792483217.h5',
+            '1e14 eV 81Mb sorted+csi': 'sorted_10019744.h5'}
+
+def do_compare(corsikafile_path):
     data_old = tables.open_file('bagger1.h5', 'w')
-    data_opt = tables.open_file('bagger2.h5', 'w') 
-    #
-    # Use GroundParticlesSimulation --> randomize azimuth and core distance
-    # we set max core distance to 0, only azimuth is varied
-    #
+    data_opt = tables.open_file('bagger2.h5', 'w')
+
     print "N = ",N
-    print "old code:"
-    sim_old = GroundParticlesSimulation(FILENAME, max_core_distance, cluster, data_old, '/', N, seed=42)
+    print "old code pytables_readwhere:"
+    sim_old = GroundParticlesSimulation(corsikafile_path, max_core_distance, cluster, data_old, '/', N, seed=42)
+
+    start = timer()
     sim_old.run()
-    testvalue_old = data_old.root.coincidences.coincidences.read_where('N>0') 
-    print "number of coincidences: ", len(testvalue_old) 	
- 
-    print "new code:"
-    sim_opt = OptimizeQueryGroundParticlesSimulation(FILENAME_SORTED, max_core_distance, cluster, data_opt, '/', N, seed=42)
+    end = timer()
+    t_old = end - start
+
+    testvalue_old = data_old.root.coincidences.coincidences.read_where('N>0')
+    print "number of coincidences: ", len(testvalue_old)
+
+    print "new code: pytbales_readwhere on x. compress on y and particle_id"
+    sim_opt = OptimizeQueryGroundParticlesSimulation(corsikafile_path, max_core_distance, cluster, data_opt, '/', N, seed=42)
+
+    start = timer()
     sim_opt.run()
-    
+    end = timer()
+    t_opt = end - start
+
     testvalue =  data_opt.root.coincidences.coincidences.read_where('N>0')
     print "number of coincidences: ", len(testvalue)
 
-    print "TEST: coincidence[0]==coincidence[0]", (testvalue_old[0]==testvalue[0]) 
+    for a,b in zip(testvalue_old,testvalue):
+        if a != b:
+            print "Coincidence not equal. Index equal? ", (a[0]==b[0]), a, b
+    data_old.close()
+    data_opt.close()
+
+    return t_old, t_opt
+
+if __name__ == '__main__':
+
+    results = {}
+    for shower_key in SHOWERS:
+        print "\n*** testcase: ", shower_key
+        results[shower_key] = do_compare(SHOWERS[shower_key])
+
+    print "\n results:"
+    for result_key in results:
+        print "Testcase: %s pytables: %3.2f s compress: %3.2f s" % (result_key,results[result_key][0], results[result_key][1])
