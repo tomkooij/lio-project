8434ed0c888d683851d74056ba59ca01c3877bd1
tomkooij <tomkooij@tomkooij.nl>
2015-05-11 14:19:39 +0200

    PEP8 and comment fixes

--- photons/compton.py
+++ photons/compton.py
@@ -4,24 +4,26 @@ Photons door scintilator
 simulatie van detector response op foton
  "nagedaan" werk van Dorrith/Jos in April 2010
 
-Formules uit W.R. Leo, "Techniques for Nucl and Part Ph experiments", Springer (1987)
+Formules uit
+  W.R. Leo, "Techniques for Nucl and Part Ph experiments", Springer (1987)
 
 """
+from __future__ import division
 
 import math
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.optimize import curve_fit
 
-E_MAX = 1000.e6 # 1000 MeV
-E_MIN = 500.e3 # 500 keV
-electron_rest_mass_MeV = .5109989 # MeV
-electron_rest_mass = electron_rest_mass_MeV * 1e6 # eV
+E_MAX = 1000.e6  # 1000 MeV
+E_MIN = 500.e3  # 500 keV
+electron_rest_mass_MeV = .5109989  # MeV
+electron_rest_mass = electron_rest_mass_MeV * 1e6  # eV
 
-THICKNESS = 2.0 # [cm] scintillator thickness
+THICKNESS = 2.0  # [cm] scintillator thickness
 
-#number of simulations
+# number of simulations
 N = 10
+
 
 #
 # Compton scattering
@@ -37,7 +39,7 @@ N = 10
 #
 def KN_total_cross_section(gamma):
 
-    r_e = 2.82e-15 # classical electron radius [m]
+    r_e = 2.82e-15  # classical electron radius [m]
 
     _1_g = 1 + gamma
     _1_2g = 1 + 2 * gamma
@@ -48,7 +50,6 @@ def KN_total_cross_section(gamma):
     return (2.*math.pi*(r_e**2) * ((_1_g/gamma**2) *
                                    ((2.*_1_g/_1_2g) - (ln_1_2g/gamma)) +
                                    (ln_1_2g/2./gamma) - (_1_3g/(_1_2g)**2)))
-
 
 
 #
@@ -65,7 +66,7 @@ def KN_total_cross_section(gamma):
 #
 def KN_scattering_cross_section(gamma):
 
-    r_e = 2.82e-15 # classical electron radius [m]
+    r_e = 2.82e-15  # classical electron radius [m]
 
     _1_g = 1 + gamma
     _1_2g = 1 + 2 * gamma
@@ -89,7 +90,7 @@ def KN_absorption_cross_section(gamma):
 
 def dsigma_dT(E,T):
 
-    r_e = 2.82e-15 # classical electron radius [m]
+    r_e = 2.82e-15  # classical electron radius [m]
 
     gamma = E / electron_rest_mass_MeV
 
@@ -99,6 +100,7 @@ def dsigma_dT(E,T):
                     (2 + (s**2 / ((gamma**2) * ((1 - s)**2)) ) +
                                 (s/(1 - s))*(s - 2/gamma)))
 
+
 # W.R. Leo (1987) p 54
 # E photon energy [MeV]
 # return compton edge [MeV]
@@ -106,7 +108,7 @@ def compton_edge(E):
 
     gamma = E / electron_rest_mass_MeV
 
-    return (E * 2 * gamma / (1 + 2*gamma) )
+    return (E * 2 * gamma / (1 + 2 * gamma))
 
 def plot_compton_cs_versus_E():
 
@@ -116,13 +118,14 @@ def plot_compton_cs_versus_E():
     # cs in barn
     cs = [KN_total_cross_section(energy / electron_rest_mass_MeV) / 1e-28 for energy in E]
 
-    plt.plot(E,cs)
+    plt.plot(E, cs)
     plt.xscale('log')
     plt.yscale('log')
     plt.ylabel('Compton scattering cross section [barn]')
     plt.xlabel('photon energy (MeV)')
     plt.title('Klein-Nisihina cross section for compton scattering')
 #    plt.savefig('kn_cross_sec.png')
+
 
 #
 # Calculate mean free path for photon in Vinyltoluene scintillator
@@ -131,16 +134,16 @@ def plot_compton_cs_versus_E():
 #
 def compton_mean_free_path(energy):
 
-    N_a = 6.022e23 # avogadro
+    N_a = 6.022e23  # avogadro
     # vinyltoluene = CH2=CHC6H4CH3 (C9H10)
-    rho = 1.032 # g/cm3
-    M = 9 * 12. + 10 * 1.
+    rho = 1.032  # g/cm3
+    A = 9 * 12. + 10 * 1.
 
     # number of atoms per unit volume (cm3)
-    n = rho * N_a / M
+    n = rho / A * N_a
 
-    # Z = atom number
-    Z = 9 * 6. + 10 * 1. # C9H10
+    # Z = atom number (= number of electrons per atom)
+    A = 9 * 6. + 10 * 1.  # C9H10
 
     # electron rest mass 0.5109989 MeV
     # cross section in [m2]
@@ -148,15 +151,16 @@ def compton_mean_free_path(energy):
 
     return 1/(n*Z*KN_total_cross_section(energy / electron_rest_mass_MeV)*1e4) # in [cm]
 
+
 #
 # Calculate the probablity of compton scattering in 2cm vinyltoluene scintilator
 #
 def interaction_probability(energy):
 
-    l = compton_mean_free_path(energy) # [cm]
+    l = compton_mean_free_path(energy)  # [cm]
 
     # P(x) = 1 - exp^(-1/l*x)  (W.R. Leo, 1987, p 20)
-    return ( 1. - math.exp(-1./l*THICKNESS) )
+    return (1. - math.exp(-1./l*THICKNESS))
 
 
 #
@@ -166,6 +170,7 @@ def compton_max_energy_transfer(energy):
 
     return (energy * (2. * energy / (electron_rest_mass_MeV+2.*energy) ) )
 #
+
 
 def plot_MAX_energy_transfer():
 
@@ -177,7 +182,7 @@ def plot_MAX_energy_transfer():
 
     # maak plotje
     plt.figure()
-    plt.plot(E,T)
+    plt.plot(E, T)
     plt.xscale('log')
     plt.yscale('log')
     plt.ylabel('T [MeV]')
@@ -185,13 +190,14 @@ def plot_MAX_energy_transfer():
     plt.title('Photon max energy loss for Compton scattering')
     #plt.savefig('T.png')
 
+
 def plot_compton_mean_free_path_versus_E():
 
     # log input [MeV]
     E = np.logspace(-3, 3, 1000)
 
     # maak een lijst met vrije weglengtes per energie
-    l = [compton_mean_free_path(energy) for energy in E] # in [cm]
+    l = [compton_mean_free_path(energy) for energy in E]  # in [cm]
 
     # maak plotje
     plt.figure()
@@ -214,9 +220,9 @@ def plot_compton_full_versus_E():
     cs_scattering = [KN_scattering_cross_section(energy / electron_rest_mass_MeV) / 1e-28 for energy in E]
     cs_absorption = [KN_absorption_cross_section(energy / electron_rest_mass_MeV) / 1e-28 for energy in E]
 
-    plt.plot(E,cs_total,label='total cs')
-    plt.plot(E,cs_scattering, label='scattering')
-    plt.plot(E,cs_absorption, label='absorption')
+    plt.plot(E, cs_total,label='total cs')
+    plt.plot(E, cs_scattering, label='scattering')
+    plt.plot(E, cs_absorption, label='absorption')
     plt.legend()
     plt.xscale('log')
     plt.yscale('log')
@@ -225,6 +231,6 @@ def plot_compton_full_versus_E():
     plt.title('Klein-Nisihina cross sections for Compton scattering')
 #    plt.savefig('kn_cross_sec.png')
 
-if __name__=='__main__':
+if __name__== '__main__':
 
     print "This is compton.py!\n"
--- photons/pair.py
+++ photons/pair.py
@@ -4,7 +4,8 @@ Photons door scintilator
 simulatie van detector response op foton
  "nagedaan" werk van Dorrith/Jos in April 2010
 
-Formules uit W.R. Leo, "Techniques for Nucl and Part Ph experiments", Springer (1987)
+Formules uit:
+W.R. Leo, "Techniques for Nucl and Part Ph experiments", Springer (1987)
 
 """
 
@@ -14,16 +15,17 @@ import math
 import numpy as np
 from matplotlib import pyplot as plt
 
-electron_rest_mass_MeV = .5109989 # MeV
-electron_rest_mass = electron_rest_mass_MeV * 1e6 # eV
+electron_rest_mass_MeV = .5109989  # MeV
+electron_rest_mass = electron_rest_mass_MeV * 1e6  # eV
 
-THICKNESS = 2.0 # [cm] scintillator thickness
+THICKNESS = 2.0  # [cm] scintillator thickness
 
 
 def f(Z):
     a = Z / 137.
 
-    return a ** 2 * (1/(1 + a ** 2) + 0.20206 - 0.03699 * a ** 2 + 0.0083 * a ** 4 - 0.002 * a ** 6)
+    return a ** 2 * (1/(1 + a ** 2) + 0.20206 - 0.03699 * a ** 2 +
+                     0.0083 * a ** 4 - 0.002 * a ** 6)
 
 
 def pair_cross_section(gamma):
@@ -32,30 +34,31 @@ def pair_cross_section(gamma):
     r_e = 2.82e-15  # classical electron radius [m]
 
     # Z = charge of an average atom  H : C = 1.104 : 1 (vinyltoluene)
-    Z = (1.104+6)/2
+    Z = (1.104+6) / 2.
 
     alpha = 1/137.
 
     _ln_fZ = math.log(2*gamma) - f(Z)
 
-    return 4 * Z * (Z+1) * alpha * r_e ** 2 * ((7. / 9.) * _ln_fZ - (109. / 54.))
+    return 4 * Z * (Z+1) * alpha * r_e ** 2 * (7. / 9 * _ln_fZ - 109. / 54)
 
 
 def pair_mean_free_path(energy):
 
-    N_a = 6.022e23 # avogadro
+    N_a = 6.022e23  # avogadro
     # vinyltoluene = CH2=CHC6H4CH3 (C9H10)
-    rho = 1.032 # g/cm3
+    rho = 1.032  # g/cm3
     A = 13.1 / 2.
 
     # number of atoms per unit volume (cm3)
-    n = rho * N_a / A
+    n = rho / A * N_a
 
     # electron rest mass 0.5109989 MeV
     # cross section in [m2] Hence the 1e-4 factor
     # n = number of atoms per unit volume [cm3]
 
-    return 1/(n*(pair_cross_section(energy / electron_rest_mass_MeV)*1e4)) # in [cm]
+    return 1/(n*(pair_cross_section(energy /
+                                    electron_rest_mass_MeV)*1e4))  # in [cm]
 
 
 def plot_pair_mean_free_path_versus_E():
@@ -64,41 +67,40 @@ def plot_pair_mean_free_path_versus_E():
     E = np.logspace(0, 3, 1000)
 
     # maak een lijst met vrije weglengtes per energie
-    l = [pair_mean_free_path(energy) for energy in E] # in [cm]
+    l = [pair_mean_free_path(energy) for energy in E]  # in [cm]
 
     # maak plotje
     plt.figure()
-    plt.plot(E,l)
+    plt.plot(E, l)
     plt.xscale('log')
     plt.yscale('log')
     plt.ylabel('mean free path - pair production [cm]')
     plt.xlabel('photon energy (MeV)')
     plt.title('Pair - mean free path in vinyltoluene scintilator')
     plt.show()
-    #plt.savefig('pair_freepath.png')
+    # plt.savefig('pair_freepath.png')
 
 
 def plot_cross_section_versus_E():
-
-    # Z = atom number
-    Z = 9 * 6. + 10 * 1. # C9H10
 
     # log input [MeV]
     E = np.logspace(0, 3, 1000)
 
     # maak een lijst met vrije weglengtes per energie
-    sigma = [pair_cross_section(energy/ electron_rest_mass_MeV)/1e-28 for energy in E] # in [barn/atom]
+    sigma = [pair_cross_section(energy /
+                                electron_rest_mass_MeV)/1e-28 for energy in E]
+    # sigma in [barn/atom]
 
     # maak plotje
     plt.figure()
-    plt.plot(E,sigma)
+    plt.plot(E, sigma)
     plt.xscale('log')
     plt.yscale('log')
     plt.ylabel('cross section - pair production [barn/atom]')
     plt.xlabel('photon energy (MeV)')
     plt.title('Pair production in vinyltoluene scintilator')
     plt.show()
-    #plt.savefig('pair_freepath.png')
+    # plt.savefig('pair_freepath.png')
 
 
 if __name__== '__main__':
