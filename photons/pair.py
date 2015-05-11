cda4c51519d56bde409f9811b3dace262846ca8c
tomkooij <tomkooij@tomkooij.nl>
2015-05-11 13:05:24 +0200

    fixed and checked cross section and free path

--- photons/pair.py
+++ photons/pair.py
@@ -23,9 +23,7 @@ THICKNESS = 2.0 # [cm] scintillator thickness
 def f(Z):
     a = Z / 137.
 
-    _1_a2 = (1 + a ** 2)
-
-    return a ** 2 * (1/(_1_a2) + 0.20206 - 0.03699 * a ** 2 + 0.0083 * a ** 4 - 0.002 * a ** 6)
+    return a ** 2 * (1/(1 + a ** 2) + 0.20206 - 0.03699 * a ** 2 + 0.0083 * a ** 4 - 0.002 * a ** 6)
 
 
 def pair_cross_section(gamma):
@@ -33,8 +31,8 @@ def pair_cross_section(gamma):
 
     r_e = 2.82e-15  # classical electron radius [m]
 
-    # Z = atom number
-    Z = 9 * 6. + 10 * 1.  # C9H10
+    # Z = charge of an average atom  H : C = 1.104 : 1 (vinyltoluene)
+    Z = (1.104+6)/2
 
     alpha = 1/137.
 
@@ -42,24 +40,23 @@ def pair_cross_section(gamma):
 
     return 4 * Z * (Z+1) * alpha * r_e ** 2 * ((7. / 9.) * _ln_fZ - (109. / 54.))
 
+
 def pair_mean_free_path(energy):
 
     N_a = 6.022e23 # avogadro
     # vinyltoluene = CH2=CHC6H4CH3 (C9H10)
     rho = 1.032 # g/cm3
-    M = 9 * 12. + 10 * 1.
+    A = 13.1 / 2.
 
     # number of atoms per unit volume (cm3)
-    n = rho * N_a / M
-
-    # Z = atom number
-    Z = 9 * 6. + 10 * 1. # C9H10
+    n = rho * N_a / A
 
     # electron rest mass 0.5109989 MeV
-    # cross section in [m2]
-    # n = number of atoms per unit volume
+    # cross section in [m2] Hence the 1e-4 factor
+    # n = number of atoms per unit volume [cm3]
 
-    return 1/(n*Z*pair_cross_section(energy / electron_rest_mass_MeV)*1e4) # in [cm]
+    return 1/(n*(pair_cross_section(energy / electron_rest_mass_MeV)*1e4)) # in [cm]
+
 
 def plot_pair_mean_free_path_versus_E():
 
@@ -80,7 +77,32 @@ def plot_pair_mean_free_path_versus_E():
     plt.show()
     #plt.savefig('pair_freepath.png')
 
+
+def plot_cross_section_versus_E():
+
+    # Z = atom number
+    Z = 9 * 6. + 10 * 1. # C9H10
+
+    # log input [MeV]
+    E = np.logspace(0, 3, 1000)
+
+    # maak een lijst met vrije weglengtes per energie
+    sigma = [pair_cross_section(energy/ electron_rest_mass_MeV)/1e-28 for energy in E] # in [barn/atom]
+
+    # maak plotje
+    plt.figure()
+    plt.plot(E,sigma)
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.ylabel('cross section - pair production [barn/atom]')
+    plt.xlabel('photon energy (MeV)')
+    plt.title('Pair production in vinyltoluene scintilator')
+    plt.show()
+    #plt.savefig('pair_freepath.png')
+
+
 if __name__== '__main__':
 
     print "This is pair.py!\n"
+    plot_cross_section_versus_E()
     plot_pair_mean_free_path_versus_E()
