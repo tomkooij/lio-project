5c540c9dfd2bec101b0dfee93001a082c4ca209d
tomkooij <tomkooij@tomkooij.nl>
2015-06-08 16:43:16 +0200

    pulseheight histogram voor lio verslag

--- pulseheight_histogram/histogram.py
+++ pulseheight_histogram/histogram.py
@@ -1,7 +1,11 @@
 import sapphire
 import datetime
 import tables
+import os
 
+import matplotlib.pyplot as plt
+
+import numpy as np
 
 STATION = 501
 STATIONS = [STATION]
@@ -28,4 +32,32 @@ def create_new_event_file(filename, stations, start, end):
 
 
 if __name__ == '__main__':
-    data = create_new_event_file(FILENAME, STATIONS, START, END)
+    if 'data' not in globals():
+        print "%s not in memory yet " % FILENAME
+        if os.path.exists(FILENAME):
+            print "%s exists. Opening." % FILENAME
+            data = tables.open_file(FILENAME, 'a')
+        else:
+            print "%s does not exist. Trying to read from ESD"
+            data = create_new_event_file(FILENAME, STATIONS, START, END)
+    else:
+        print "data already in globals."
+
+    events = data.root.s501.events
+
+    ph = events.col('pulseheights')
+    ph1 = ph[:, 0]
+    ph2 = ph[:, 1]
+    ph3 = ph[:, 2]
+    ph4 = ph[:, 3]
+
+    plt.xkcd()
+    plt.figure()
+    plt.hist(ph1, bins=np.arange(10.,1200.,20.), histtype='step')
+    plt.title('/lio-project/pulseheight_histogram/histogram.py')
+    #plt.legend('[s501. detector 1. 2010-4-1 24h]')
+    plt.xlabel('pulshoogte [ADC]')
+    plt.ylabel('aantal')
+    plt.savefig('histogram.py.xkcd.png',dpi=150)
+    plt.show()
+    data.close()
