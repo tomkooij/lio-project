456b2ea72caf8c6d8bce7d7e218fd43f1fe8b48a
tomkooij <tomkooij@tomkooij.nl>
2015-11-09 16:25:45 +0100

    better selections

--- s102/s102.py
+++ s102/s102.py
@@ -4,11 +4,15 @@ from matplotlib.colors import LogNorm
 import tables
 from sapphire import Station
 import random
+import os
 
+PLOT_TRACES = False   # download en plot traces
+TXTOUTFILE = 's102_1laag_2hoog.txt'
 FILENAME = 's102_heel2015.h5'
 
-LOW_PH = 120
-HIGH_PH = 200
+MIP = 150 / 0.57  # ADC =  mV/0.57
+LOW_PH = 0.5*MIP
+HIGH_PH = 1.*MIP
 
 
 def plot_trace(station_id, ext_timestamp, t1, t2, start=625, end=1250):
@@ -48,17 +52,20 @@ if __name__ == '__main__':
     ph1 = ph[:, 0]
     ph2 = ph[:, 1]
 
-    bins2ns5 = np.arange(-101.25, 101.26, 2.5)
+    bins2ns5 = np.arange(-251.25, 251.26, 5.)
 
     grafiek = plt.figure()
 
     # remove -1 and -999
     # select events based on pulseheight
-    filter = (t1 >= 0) & (t2 >= 0) & (ph2 > 0) & (ph2 < LOW_PH) & (ph1 > HIGH_PH)
-    dt = (t1-t2).compress(filter)
-    t1_ = t1.compress(filter)
-    t2_ = t2.compress(filter)
-    stamps = ext_stamp.compress(filter)
+    filter = (t1 >= 0) & (t2 >= 0) & (ph1 > 0) & (ph1 < LOW_PH) & (ph2 > HIGH_PH)
+    t1_filtered = t1.compress(filter)
+    t2_filtered = t2.compress(filter)
+    ph1_filtered = ph1.compress(filter)
+    ph2_filtered = ph2.compress(filter)
+    timestamp_filtered = ext_stamp.compress(filter)
+
+    dt = t1_filtered - t2_filtered
 
     print "number of events in hoog/laag filter:", dt.size
     n1, bins1, blaat1 = plt.hist(dt, bins=bins2ns5, histtype='step')
@@ -67,23 +74,26 @@ if __name__ == '__main__':
     plt.show()
 
     plt.figure()
-    plt.hist2d(t1_ ,t2_, bins=40, norm=LogNorm())
+    plt.hist2d(t1_filtered ,t2_filtered, bins=40, norm=LogNorm())
     plt.colorbar()
+    plt.savefig('2dhist.png', dpi=200)
     plt.show()
 
+    dt_filtered = t2_filtered - t1_filtered
     #selection = (t1_ > 1000) & (t2_ < 985)
-    selection = (t1_ < 985) & (t2_ > 1000)
+    selection = (t1_filtered > 1000) & (t1_filtered < 1080) & (dt_filtered < -40) & (dt_filtered > -80)
 
-    selected_stamps = stamps.compress(selection)
-    t1_selection = t1_.compress(selection)
-    t2_selection = t2_.compress(selection)
+    selected_stamps = timestamp_filtered.compress(selection)
+    t1_selection = t1_filtered.compress(selection)
+    t2_selection = t2_filtered.compress(selection)
     print "number of events in selection:", selected_stamps.size
 
-    for i in range(3):
-        # random.choice and np.random.choice does not work on uint64
-        idx = random.randint(0, selected_stamps.size-1)
-        event_timestamp = selected_stamps[idx]
-        event_t1 = t1_selection[idx]
-        event_t2 = t2_selection[idx]
-        print i, event_timestamp
-        traces = plot_trace(102, event_timestamp, event_t1, event_t2, 500, 1500)
+    if (PLOT_TRACES):
+        for i in range(10):
+            # random.choice and np.random.choice does not work on uint64
+            idx = random.randint(0, selected_stamps.size-1)
+            event_timestamp = selected_stamps[idx]
+            event_t1 = t1_selection[idx]
+            event_t2 = t2_selection[idx]
+            print i, event_timestamp
+            traces = plot_trace(102, event_timestamp, event_t1, event_t2, 900, 1500)
