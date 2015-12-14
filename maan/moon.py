5e6ea8d4d33d6bfb870cf473200bbf6bd4e57fa3
tomkooij <tomkooij@tomkooij.nl>
2015-12-14 16:25:31 +0100

    compute distance from event to moon " by hand\

--- maan/moon.py
+++ maan/moon.py
@@ -1,8 +1,13 @@
 # ephem
 from datetime import datetime
 import ephem
+import numpy as np
 
+# from 501 events
 timestamp = 1264368023
+zenith = 0.73978639
+azimuth = -2.68735647 % (2*np.pi)
+
 
 #
 # SciencePark
@@ -16,3 +21,11 @@ SciencePark.date = datetime.utcfromtimestamp(timestamp)
 m = ephem.Moon(SciencePark)
 print SciencePark
 print "m = Moon(SciencePark @ %s) " % datetime.utcfromtimestamp(timestamp)
+
+moon_zenith = np.pi/2 - float(m.alt)
+moon_azimuth = float(m.az) % (2*np.pi)
+
+print
+print "event (zenith, azimuth): ", zenith, azimuth
+print "moon (zenith, azimuth): ", moon_zenith, moon_azimuth
+print "separation from (alt,az) = %f" % np.sqrt((zenith-moon_zenith)**2 + (azimuth - moon_azimuth)**2)
