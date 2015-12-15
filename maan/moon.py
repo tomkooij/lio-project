871614627409dee4812e63214882c3979b647ddd
tomkooij <tomkooij@tomkooij.nl>
2015-12-15 08:47:02 +0100

    add law of cosines angle separation

--- maan/moon.py
+++ maan/moon.py
@@ -4,9 +4,23 @@ import ephem
 import numpy as np
 
 from sapphire.transformations.celestial import zenithazimuth_to_equatorial
-from sapphire.transformations.base import decimal_to_sexagesimal as decdeg2dms
+from sapphire.transformations.base import decimal_to_sexagesimal
 from sapphire.utils import norm_angle, angle_between
 
+
+def dms(angle):
+    """ convert decimal angle to dms string 00d00m00.00s"""
+    d,m,s = decimal_to_sexagesimal(angle)
+    return "%dd%dm%2.2f" % (d,m,s)
+
+def law_of_cosines(lat1, lon1, lat2, lon2):
+    """ law of cosines angle between angles
+    angles (lat,lon) in degrees and/or radians
+
+    http://gis.stackexchange.com/questions/4906/why-is-law-of-cosines-more-preferable-than-haversine-when-calculating-distance-b
+    """
+
+    return np.sin(lat1)*np.sin(lat2)+np.cos(lat1)*np.cos(lat2)*np.cos(lon2-lon1)
 
 # event
 timestamp = 1264368023
@@ -53,8 +67,9 @@ print "moon (alt, azi) [degrees]", 90. - np.degrees(moon_zenith), np.degrees(moo
 print "event (zenith, azimuth): ", zenith, azimuth
 print "moon (zenith, azimuth): ", moon_zenith, moon_azimuth
 print
-print "moon (ra [graden] , dec [uren])", decdeg2dms(moon_ra/(2*np.pi)*24), decdeg2dms(np.degrees(moon_dec))
-print "event (ra,dec) calculated from zen,az,position", decdeg2dms(ra/(2*np.pi)*24), decdeg2dms(np.degrees(dec))
+print "moon (ra [graden] , dec [uren])", dms(moon_ra/(2*np.pi)*24), dms(np.degrees(moon_dec))
+print "event (ra,dec) calculated from zen,az,position", dms(ra/(2*np.pi)*24), dms(np.degrees(dec))
 print
-print "separation from (alt,az) = %f" % angle_between(zenith, azimuth, moon_zenith, moon_azimuth)
+print "separation (haversine) from (alt,az) = %f" % angle_between(zenith, azimuth, moon_zenith, moon_azimuth)
+print "separation (law of cosines) from (alt, az) = %f" % law_of_cosines(zenith, azimuth, moon_zenith, moon_azimuth)
 print "seperation from ephem.separation((ra,dec),(moon_ra,moon_dec))", float(ephem.separation((ra,dec),(moon_ra,moon_dec)))
