c3eb669edde30af22cc13f40a6b61058bc7110c9
tomkooij <tomkooij@tomkooij.nl>
2015-12-21 19:20:08 +0100

    path for stoomboot

--- dt-sim/single_sim_for_stoomboot.py
+++ dt-sim/single_sim_for_stoomboot.py
@@ -9,13 +9,13 @@ from sapphire.clusters import SingleTwoDetectorStation
 import tables
 
 max_r = 100
-N = 1e5
-cluster = SingleDiamondStation()
+N = int(1e5)
+cluster = SingleTwoDetectorStation()
 
-CORSIKAFILE = '/data/hisparc/corsika/data/{seed}/corsika.h5'
-OUTPUTFILE = '/data/hisparc/tom/simruns/twodetector/{seed}.h5'
+CORSIKAFILE = '/data/hisparc/corsika/data/{seed}/corsika.h5'.format(seed=seed)
+OUTPUTFILE = '/data/hisparc/tom/simruns/twodetector/{seed}.h5'.format(seed=seed)
 
 with tables.open_file(OUTPUTFILE, 'w') as data:
-    sim = GroundParticlesSimulation(CORSIKAFILE, max_r, cluster, data, '/simrun', N, progress=False)
+    sim = GroundParticlesSimulation(CORSIKAFILE, max_r, cluster, data, '/simrun', N)
     sim.run()
     sim.finish()
