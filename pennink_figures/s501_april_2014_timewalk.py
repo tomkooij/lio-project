e46ee72b9c701b2cb7f8189d99049e5fe013d1b4
tomkooij <tomkooij@tomkooij.nl>
2015-12-21 19:17:21 +0100

    investigate timewalk correction

--- pennink_figures/s501_april_2014_timewalk.py
+++ pennink_figures/s501_april_2014_timewalk.py
@@ -9,7 +9,8 @@ ph1 < TRIGGER = gamma
 
 """
 
-import gauss_fit_histogram
+from gauss_fit_histogram import gauss_fit_histogram
+
 import datetime
 import tables
 import sapphire.esd
@@ -32,8 +33,8 @@ FILENAME = 'station_501_april2014.h5'
 # <120 ADC counts = gamma
 # These values are consistent with a pulseheight histogram
 #
-HIGH_PH = 200
-LOW_PH = 120
+HIGH_PH = 300
+LOW_PH = 150
 
 #
 # Read event data from the ESD
@@ -68,10 +69,17 @@ if __name__=='__main__':
 
     events = data.root.s501.events
 
-    t1 = events.col('t1')
-    t2 = events.col('t2')
+    t1 = events.col('t4')
+    t2 = events.col('t1')
     t3 = events.col('t3')
     t4 = events.col('t4')
+
+    OFFSET = 7.  # ns
+    if OFFSET:
+        t1 = t1 - OFFSET
+        #t2 = t2 - OFFSET
+        #t3 = t3 - OFFSET
+        #t4 = t4 - OFFSET
 
     ph = events.col('pulseheights')
     ph1 = ph[:,0]
@@ -82,46 +90,48 @@ if __name__=='__main__':
     bins2ns5 = np.arange(-HIST_WIDTH-1.25,HIST_WIDTH+1.26,2.5)
 
     dt_all = (t1-t2).compress( (t1 >0) & (t2 > 0) )
+    dt_t2hoog_t2hoog = (t1-t2).compress( (t1 >0) & (t2 > 0) & (ph1 > HIGH_PH) & (ph2 > HIGH_PH))
     dt_t1hoog_t2laag = (t1-t2).compress( (t1 >0) & (t2 > 0) & ( (t1-t2) < 60. ) & (ph1 > HIGH_PH) & (ph2 < LOW_PH) )
     dt_t2laag_t2hoog = (t1-t2).compress( (t1 >0) & (t2 > 0) & ( (t1-t2) < 60. ) & (ph2 > HIGH_PH) & (ph1 < LOW_PH) )
-    dt_t2laag_t2laag = (t1-t2).compress( (t1 >0) & (t2 > 0) & (ph1 < LOW_PH) & (ph2 < LOW_PH))
+    dt_t2laag_t2laag = (t1-t2).compress( (t1 >0) & (t2 > 0) & (ph1 > 20.) & (ph2 > 20.) & (ph1 < LOW_PH) & (ph2 < LOW_PH))
 
-    print "All events", dt_all.size
-    n1, bins1, blaat1 = plt.hist(dt_all, bins=bins2ns5, histtype='step')
+    if 0:
+        print "All events", dt_all.size
+        n1, bins1, blaat1 = plt.hist(dt_all, bins=bins2ns5, histtype='step')
+
+        sigma_list = np.sqrt(n1)
+
+        c, fitx, fity = gauss_fit_histogram(n1, bins1, sigma=sigma_list, initialguess = [100.,10., 10., 0.], verbose=True)
+        mu = c[1]
+        sigma = abs(c[2])
+
+        plt.plot(fitx, fity ,'r--', linewidth=3)
+        plt.title('%s t1-t2: ph1,ph2=hoog' % FILENAME)
+        plt.xlabel('pulseheight [ADC]')
+        plt.legend([r'fit: $ \mu = %.3f\  \sigma = %.3f\ $' %(mu, sigma), 't1-t2' ], loc = 2, bbox_to_anchor=(1,1))
+        plt.show()
+
+        print "ph1 hoog, ph2 laag", dt_t1hoog_t2laag.size
+        n1, bins1, blaat1 = plt.hist(dt_t1hoog_t2laag, bins=bins2ns5, histtype='step')
+        plt.title('t1-t2: ph1=hoog, ph2=laag')
+        plt.show()
+
+        print "ph1 laag, ph1 hoog", dt_t2laag_t2hoog.size
+        n1, bins1, blaat1 = plt.hist(dt_t2laag_t2hoog, bins=bins2ns5, histtype='step')
+        plt.title('t1-t2: ph1=laag, ph2=hoog')
+        plt.show()
+
+    print "ph2 laag, ph2 laag", dt_t2hoog_t2hoog.size
+    n1, bins1, blaat1 = plt.hist(dt_t2hoog_t2hoog, bins=bins2ns5, histtype='step')
 
     sigma_list = np.sqrt(n1)
 
-    c, fitx, fity = gauss_fit_histogram.gauss_fit_histogram(n1, bins1, sigma=sigma_list, initialguess = [100.,10., 10., 0.], verbose=True)
+    c, fitx, fity = gauss_fit_histogram(n1, bins1, sigma=sigma_list, initialguess = [100.,10., 10., 0.], verbose=True)
     mu = c[1]
     sigma = abs(c[2])
 
     plt.plot(fitx, fity ,'r--', linewidth=3)
     plt.title('%s t1-t2: ph1,ph2=hoog' % FILENAME)
-    plt.xlabel('pulseheight [ADC]')
-    plt.legend([r'fit: $ \mu = %.3f\  \sigma = %.3f\ $' %(mu, sigma), 't1-t2' ], loc = 2, bbox_to_anchor=(1,1))
-    plt.show()
-
-    print "ph1 hoog, ph2 laag", dt_t1hoog_t2laag.size
-    n1, bins1, blaat1 = plt.hist(dt_t1hoog_t2laag, bins=bins2ns5, histtype='step')
-    plt.title('t1-t2: ph1=hoog, ph2=laag')
-    plt.show()
-
-    print "ph1 laag, ph1 hoog", dt_t2laag_t2hoog.size
-    n1, bins1, blaat1 = plt.hist(dt_t2laag_t2hoog, bins=bins2ns5, histtype='step')
-    plt.title('t1-t2: ph1=laag, ph2=hoog')
-    plt.show()
-
-    print "ph2 laag, ph2 laag", dt_t2laag_t2laag.size
-    n1, bins1, blaat1 = plt.hist(dt_t2laag_t2laag, bins=bins2ns5, histtype='step')
-
-    sigma_list = np.sqrt(n1)
-
-    c, fitx, fity = gauss_fit_histogram.gauss_fit_histogram(n1, bins1, sigma=sigma_list, initialguess = [100.,10., 10., 0.], verbose=True)
-    mu = c[1]
-    sigma = abs(c[2])
-
-    plt.plot(fitx, fity ,'r--', linewidth=3)
-    plt.title('%s t1-t2: ph1,ph2=laag' % FILENAME)
     plt.xlabel('pulseheight [ADC]')
     plt.legend([r'fit: $ \mu = %.3f\  \sigma = %.3f\ $' %(mu, sigma), 't1-t2' ], loc = 2, bbox_to_anchor=(1,1))
     plt.show()
@@ -132,17 +142,17 @@ if __name__=='__main__':
 
     # time walk correction function
     # fit_clas_note_02_2007.py:
-    t_walk = lambda x: -3.944 + 22.3 / (x-20.)**0.156
+    t_walk = lambda x: -3. + (22.3 / (x-20.)**0.156)
 
     t1_corr = t1 - t_walk(ph1)
-    t2_corr = t2 - t_walk(ph2)
-    dt_corr = (t1_corr-t2_corr).compress( (t1 >0) & (t2 > 0) & (ph1 < LOW_PH) & (ph2 < LOW_PH))
+    t2_corr = t2 - 20 #- t_walk(ph2)
+    dt_corr = (t1_corr-t2_corr).compress( (t1 >0) & (t2 > 0) & (ph1 > 20.) & (ph2 > 20.) & (ph1 < LOW_PH) & (ph2 < LOW_PH))
 
     n1, bins1, blaat1 = plt.hist(dt_corr, bins=bins2ns5, histtype='step')
 
     sigma_list = np.sqrt(n1)
 
-    c, fitx, fity = gauss_fit_histogram.gauss_fit_histogram(n1, bins1, sigma=sigma_list, initialguess = [100.,10., 10., 0.], verbose=True)
+    c, fitx, fity = gauss_fit_histogram(n1, bins1, sigma=sigma_list, initialguess = [100.,10., 10., 0.], verbose=True)
     mu = c[1]
     sigma = abs(c[2])
 
