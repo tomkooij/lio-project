ec356c56fcfa83590c91eefca640fc387661285b
tomkooij <tomkooij@tomkooij.nl>
2016-01-21 11:12:51 +0100

    script for reconstruction of simulated events from topaz151013_cluster_efficiency

---
+++ core/cluster_reconstructions.py
@@ -0,0 +1,46 @@
+from __future__ import division
+
+import glob
+
+import tables
+
+from sapphire import ReconstructESDEvents, ReconstructESDCoincidences
+from sapphire.utils import pbar
+
+#PATHS = '/Users/arne/Datastore/cluster_efficiency/151013*.h5'
+PATHS = '*.h5'
+
+
+def reconstruct_simulations(path):
+    with tables.open_file(path, 'a') as data:
+        cluster = data.root.coincidences._v_attrs.cluster
+
+        print "Reconstruct each station"
+        # Reconstruct each station
+        for station in cluster.stations:
+            print "Reconstructing station: ", station.number
+            station_group = '/cluster_simulations/station_%d' % station.number
+            rec_events = ReconstructESDEvents(data, station_group, station,
+                                              overwrite=True, progress=False)
+            rec_events.prepare_output()
+            rec_events.offsets = [d.offset for d in station.detectors]
+            rec_events.store_offsets()
+            rec_events.reconstruct_directions()
+            rec_events.store_reconstructions()
+
+        print "Reconstructiong coincidences:"
+        # Reconstruct coincidences
+        rec_coins = ReconstructESDCoincidences(data, '/coincidences',
+                                               overwrite=True, progress=False)
+        rec_coins.prepare_output()
+        rec_coins.offsets = {station.number: [d.offset + station.gps_offset
+                                              for d in station.detectors]
+                             for station in cluster.stations}
+        rec_coins.reconstruct_directions()
+        rec_coins.store_reconstructions()
+
+
+if __name__ == "__main__":
+    for path in pbar(glob.glob(PATHS)):
+        print 'Now processing: ', path
+        reconstruct_simulations(path)
