802990a901f4eea337210d3304c710184fee68cc
Tom Kooij <tkooij@nikhef.nl>
2016-01-22 09:21:46 +0100

    do not reconstruct events, only coincidences reconstruct directions and cores skip hdf5 files with reconstructions

--- core/cluster_reconstructions.py
+++ core/cluster_reconstructions.py
@@ -8,36 +8,30 @@ from sapphire import ReconstructESDEvents, ReconstructESDCoincidences
 from sapphire.utils import pbar
 
 #PATHS = '/Users/arne/Datastore/cluster_efficiency/151013*.h5'
-PATHS = '*.h5'
+PATHS = '/data/hisparc/tom/grid/*.h5'
 
 
 def reconstruct_simulations(path):
     with tables.open_file(path, 'a') as data:
         cluster = data.root.coincidences._v_attrs.cluster
 
-        print "Reconstruct each station"
-        # Reconstruct each station
-        for station in cluster.stations:
-            print "Reconstructing station: ", station.number
-            station_group = '/cluster_simulations/station_%d' % station.number
-            rec_events = ReconstructESDEvents(data, station_group, station,
-                                              overwrite=True, progress=False)
-            rec_events.prepare_output()
-            rec_events.offsets = [d.offset for d in station.detectors]
-            rec_events.store_offsets()
-            rec_events.reconstruct_directions()
-            rec_events.store_reconstructions()
+        try:
+            _ = data.getNode('/coincidences/reconstructions')
+            print 'Reconstructions exist. Skipping'
+            return False
 
-        print "Reconstructiong coincidences:"
-        # Reconstruct coincidences
-        rec_coins = ReconstructESDCoincidences(data, '/coincidences',
+        except tables.NoSuchNodeError: 
+            print "Reconstructiong coincidences:"
+            # Reconstruct coincidences
+            rec_coins = ReconstructESDCoincidences(data, '/coincidences',
                                                overwrite=True, progress=False)
-        rec_coins.prepare_output()
-        rec_coins.offsets = {station.number: [d.offset + station.gps_offset
+            rec_coins.prepare_output()
+            rec_coins.offsets = {station.number: [d.offset + station.gps_offset
                                               for d in station.detectors]
                              for station in cluster.stations}
-        rec_coins.reconstruct_directions()
-        rec_coins.store_reconstructions()
+            rec_coins.reconstruct_directions()
+            rec_coins.reconstruct_cores()
+            rec_coins.store_reconstructions()
 
 
 if __name__ == "__main__":
