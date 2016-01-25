5926e2f7c13469f7966027ba5677808fe3999f55
Tom Kooij <tkooij@nikhef.nl>
2016-01-25 15:57:30 +0100

    core reconstruction tests from tevere

--- core/plot_reconstruction_accuracy.py
+++ core/plot_reconstruction_accuracy.py
@@ -14,7 +14,9 @@ import matplotlib.pyplot as plt
 
 from sapphire.utils import pbar
 
-PATHS = '/data/hisparc/tom/science_park/*.h5'
+#PATHS = '/data/hisparc/tom/grid/1094*.h5'
+PATHS = '/data/hisparc/tom/grid/5x5/4278*h5'
+
 LOCALPATH = '2*.h5'
 #STATIONS = [501, 502, 503, 504, 505, 506, 508, 509]
 
@@ -42,7 +44,7 @@ def get_combined_results():
             filtered_recs = recs.read() #_where(station_query)
             print 'number of rows in query: ', len(filtered_recs)
             zenith.extend(degrees(filtered_recs['zenith']))
-            zenith_in.extend(degrees(filtered_recs['reference_zenith']))
+            zenith_in.extend(degrees(filtered_recs['zenith']))
             energy_in.extend(log10(filtered_recs['energy']))
             size_in.extend(log10(filtered_recs['size']))
             r_in.extend(sqrt(filtered_recs['x']**2 + filtered_recs['y']**2))
@@ -55,7 +57,6 @@ def get_combined_results():
 
     zenith = array(zenith)
     print "%d events before zenith is not NaN filter" % zenith.size
-    """
     filter = ~isnan(zenith)
 
     zenith = zenith.compress(filter)
@@ -65,18 +66,17 @@ def get_combined_results():
     energy_in = array(energy_in).compress(filter)
     size_in = array(size_in).compress(filter)
     r_in = array(r_in).compress(filter)
-    """
 
-    zenith_init = array(zenith_init)
+    zenith_init = array(zenith_init).compress(filter)
     energy_init = array(energy_init)
     size_init = array(size_init)
-    r_init = array(r_init)
+    r_init = array(r_init).compress(filter)
 
 
     return (zenith, zenith_in, energy_in, size_in, r_in,
             zenith_init, energy_init, size_init, r_init)
 
-def plot_reconstruction_error(r_reconstructed, r):
+def plot_core_reconstruction_error(r_reconstructed, r):
 
     print "%d events: " % len(r)
 
@@ -86,11 +86,30 @@ def plot_reconstruction_error(r_reconstructed, r):
     delta_r = abs(r_reconstructed - r)
 
     plt.figure()
-    plt.title('11x11 (50mx50m) grid: Core reconstruction error')
+    plt.title('5x5 (40mx40m) grid: Core reconstruction error')
     plt.ylabel('delta R [m]')
     plt.xlabel('R [m] = distance from center of grid')
     plt.scatter(r, delta_r)
-    plt.plot([50,50],[0,100])
+    plt.xlim(0,50)
+    plt.ylim(0,50)
+    plt.plot([20,20],[0,100])
+    plt.show()
+
+def plot_angle_reconstruction_error(z_reconstructed, z):
+    
+    delta_z = abs(z_reconstructed - z)
+    plt.figure()
+    
+
+    plt.figure()
+    plt.title('5x5 (40mx40m) grid: Angle reconstruction error')
+    plt.ylabel('delta zenith [degrees]')
+    plt.xlabel('zenith [degrees] = distance from center of grid')
+    plt.scatter(z, delta_z)
+    plt.xlim(0,60)
+    plt.show()
+
+
 
 
 
@@ -102,3 +121,6 @@ if __name__ == "__main__":
 
     zenith, zenith_in, energy_in, size_in, r_in, zenith_init, energy_init, \
         size_init, r_init = get_combined_results()
+
+    plot_core_reconstruction_error(r_in, r_init)
+    plot_angle_reconstruction_error(zenith, zenith_in)
