a3ad3d3107d915f93a45ff016d50209852decb92
tomkooij <tomkooij@tomkooij.nl>
2016-02-01 19:59:14 +0100

    make sure to get data from server

--- driehoeken/network.py
+++ driehoeken/network.py
@@ -2,20 +2,20 @@ from sapphire import Network, Station
 from sapphire.utils import pbar
 import json
 
-def get_all_stations():
+def get_all_station_numbers():
     network = Network()
-    all_station_info = Network().stations()
+    all_stations = Network().stations()
 
-    stations = [station['number'] for station in all_station_info]
+    stations = [station['number'] for station in all_stations]
     return stations
 
 def get_station_locations(stations):
     """ get station locations from API add station id """
     station_locations = []
 
-    for station in pbar(stations[0:5]):
+    for station in pbar(stations):
         try:
-            loc = Station(station).location()
+            loc = Station(station,allow_stale=False).location()
         except:
             loc = {}
         loc['number'] = station
@@ -23,7 +23,7 @@ def get_station_locations(stations):
     return station_locations
 
 if __name__ == '__main__':
-    stations = get_all_stations()
+    stations = get_all_station_numbers()
     station_locations = get_station_locations(stations)
     with open('locations.json','w') as f:
         json.dump(station_locations, f)
