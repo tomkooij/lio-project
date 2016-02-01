7f43ef71e15f615cb60c6850e3bc8a8fdd23a69e
tomkooij <tomkooij@tomkooij.nl>
2016-02-01 20:42:34 +0100

    use Network().station_numbers(). Plot map

--- driehoeken/network.py
+++ driehoeken/network.py
@@ -1,13 +1,7 @@
 from sapphire import Network, Station
 from sapphire.utils import pbar
 import json
-
-def get_all_station_numbers():
-    network = Network()
-    all_stations = Network().stations()
-
-    stations = [station['number'] for station in all_stations]
-    return stations
+import matplotlib.pyplot as plt
 
 def get_station_locations(stations):
     """ get station locations from API add station id """
@@ -16,14 +10,30 @@ def get_station_locations(stations):
     for station in pbar(stations):
         try:
             loc = Station(station,allow_stale=False).location()
+            loc['number'] = station
+            station_locations.append(loc)
         except:
-            loc = {}
-        loc['number'] = station
-        station_locations.append(loc)
+            print 'API call for station %d failed. Skipping.' % station
+
     return station_locations
 
+def plot_map(x,y):
+    plt.figure()
+    plt.plot(x,y,'ro')
+    plt.ylim(min(y)-1,max(y)+1)
+    plt.xlim(min(x)-1,max(x)+1)
+    plt.show()
+
 if __name__ == '__main__':
-    stations = get_all_station_numbers()
-    station_locations = get_station_locations(stations)
-    with open('locations.json','w') as f:
-        json.dump(station_locations, f)
+    stations = Network().station_numbers()
+    #station_locations = get_station_locations(stations)
+    #with open('locations.json','w') as f:
+    #    json.dump(station_locations, f)
+
+    with open('locations.json') as f:
+        station_locations = json.load(f)
+
+    y = [station['latitude'] for station in station_locations]
+    x = [station['longitude'] for station in station_locations]
+
+    plot_map()
