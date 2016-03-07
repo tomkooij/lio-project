4296506154f0789992e137127b1eafee48a3fa1d
Tom Kooij <tomkooij@tomkooij.nl>
2016-03-07 13:17:59 +0100

    fix missing BASE url

--- driehoeken/cluster_uptime.py
+++ driehoeken/cluster_uptime.py
@@ -10,6 +10,7 @@ import numpy as np
 import datetime
 from sapphire.transformations.clock import datetime_to_gps
 
+BASE = 'http://data.hisparc.nl/show/source/eventtime/%d/' 
 PATH = '../Datastore/publicdb/eventtime/'
 
 def process_time(time):
